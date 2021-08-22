8d78b84b1d235989b77245f0222d7d0a6f51231d
NOT SET <NOTSET@NOMAIL.COM>
2021-08-22 22:48:18 +0200

    App version 1.0.0

---
+++ app/inject.js
@@ -0,0 +1,21 @@
+/*
+ * Hide BigQuery Explorer Panel
+ * Project homepage: https://arcq.de/hide-bigquery-explorer-panel/
+ * Github: <Github link coming soon...>
+ * 
+ * MIT License
+ * Sebastian Arcq
+ */
+
+(function() {
+
+	if (window.location.href.includes("console.cloud.google.com/bigquery") == true) {
+        sidebar_status = document.querySelectorAll("cfc-panel.cfc-panel-color-grey.cfc-panel-orientation-vertical")[0];
+        if (sidebar_status.style.display == 'none') {
+            sidebar_status.style.display = 'unset'
+        } else {
+            sidebar_status.style.display = 'none'
+        }
+    }
+
+})();
