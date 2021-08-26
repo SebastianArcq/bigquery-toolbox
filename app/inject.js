9a2e6ac63db1515d0ab9fc907998862b5273b641
Sebastian Arcq <NOTSET@NOMAIL.COM>
2021-08-26 21:56:45 +0200

    changed default .display value from "none" to "flex". v1.0.1

--- app/inject.js
+++ app/inject.js
@@ -12,7 +12,7 @@
 	if (window.location.href.includes("console.cloud.google.com/bigquery") == true) {
         sidebar_status = document.querySelectorAll("cfc-panel.cfc-panel-color-grey.cfc-panel-orientation-vertical")[0];
         if (sidebar_status.style.display == 'none') {
-            sidebar_status.style.display = 'unset'
+            sidebar_status.style.display = 'flex'
         } else {
             sidebar_status.style.display = 'none'
         }
