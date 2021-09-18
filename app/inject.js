0db21e325a07ac738fc77891309e8841235763c0
Sebastian Arcq <NOTSET@NOMAIL.COM>
2021-09-19 01:00:03 +0200

    Now also hides query results + rewritten logic

--- app/inject.js
+++ app/inject.js
@@ -7,28 +7,49 @@
  * Sebastian Arcq
  */
 
-console.log("executing inject.js"); // --> browser console
+console.log("executing inject.js..."); // --> log to browser console
 
 (function() {
 
 	if (window.location.href.includes("console.cloud.google.com/bigquery") == true) {
-        // Hide BQ exlorer (containing projects, tables)
-        // <cfc-panel ... class="cfc-panel cfc-panel-color-grey cfc-panel-orientation-vertical" ...>
-        sidebar_status = document.querySelectorAll("cfc-panel.cfc-panel-color-grey.cfc-panel-orientation-vertical")[0];
-        if (sidebar_status.style.display == 'none') {
-            sidebar_status.style.display = 'flex'
-        } else {
-            sidebar_status.style.display = 'none'
-        }
+
+        // Declare relevant objects
+        // BQ exlorer (containing projects, tables)
+        sidebar = document.querySelectorAll("cfc-panel.cfc-panel-color-grey.cfc-panel-orientation-vertical")[0];
 
         // Hide top toolbar ("Features & Info" / "Shortcut" / "Disable Editor Tabs")
-        // <mat-toolbar ... class="mat-toolbar cfc-action-bar md-menu-toolbar cfc-legacy-toolbar-resize mat-toolbar-single-row" ...>
-        top_toolbar_status = document.querySelectorAll("mat-toolbar.cfc-action-bar.md-menu-toolbar.cfc-legacy-toolbar-resize.mat-toolbar-single-row")[0];
-        if (top_toolbar_status.style.display == 'none') {
-            top_toolbar_status.style.display = 'flex'
-        } else {
-            top_toolbar_status.style.display = 'none'
+        top_toolbar = document.querySelectorAll("#default-action-bar > mat-toolbar")[0];
+
+        // Hide query results
+        query_results= document.querySelectorAll("cfc-panel.cfc-panel.cfc-panel-color-grey.cfc-panel-orientation-horizontal")[0];
+
+        // Initialize hide_status
+        if (typeof hide_status == 'undefined') {
+            hide_status = 'not_hidden';
         }
-    }
+        console.log("hide_status: " + hide_status);
+
+        // Hide / Unhide
+        
+        if (hide_status == "not_hidden") {
+            sidebar.style.display = 'none'; // hide sidebar
+            top_toolbar.style.display = 'none'; // hide top toolbar
+            console.log('pausing for a bit...'); // the height is changed when the other elements are hidden. Needs a few ms to update.
+            setTimeout(function() {
+                query_results.style.height = '100%'; // hide query results
+            }, (300));
+            console.log('done, continuing');
+            hide_status = "hidden" // update hide_status
+            console.log("hide_status changed to: " + hide_status);
+
+        } else {
+            sidebar.style.display = 'flex'; // unhide sidebar
+            top_toolbar.style.display = 'flex'; // unhide top toolbar
+            query_results.style.height = '50%'; // unhide query results
+            hide_status = 'not_hidden'; // update hide_status
+            console.log("hide_status changed to: " + hide_status);
+        }
+
+    };
 
 })();
